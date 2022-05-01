e69c90d132f857f4594c37e90ea3e264b727441d
rmman11 <38256101+rmman11@users.noreply.github.com>
2022-05-01 07:12:55 +0100

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,10 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -14,6 +18,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+
+      $faker = \Faker\Factory::create();
+   for ($i=0; $i < 5; $i++) {
+      DB::table('admin_users')->insert([
+         'username' => $faker->word,
+         'email' => $faker->unique()->email,
+        'remember_token' => str::random(50),
+         'avatar' =>$faker->image('public/uploads',640,480, null, false),
+         'password' => bcrypt('secret123'),
+         'created_at' => date('Y-m-d H:i:s'),
+         'updated_at' => date('Y-m-d H:i:s'),
+     ]);
+       }
     }
 }
